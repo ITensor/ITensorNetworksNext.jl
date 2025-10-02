eeac23b925c17992914a7b3eb8963f9fbbf24f19
Joey <joeytindall94@googlemail.com>
2025-10-02 13:25:12 -0400

    Working BP Commit

--- src/ITensorNetworksNext.jl
+++ src/ITensorNetworksNext.jl
@@ -3,4 +3,7 @@ module ITensorNetworksNext
 include("abstracttensornetwork.jl")
 include("tensornetwork.jl")
 
+include("beliefpropagation/abstractbeliefpropagationcache.jl")
+include("beliefpropagation/beliefpropagationcache.jl")
+
 end
