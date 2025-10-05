5dcecfb9fffb0167343048b074effccde4d70db4
Joey <joeytindall94@googlemail.com>
2025-10-05 10:54:41 -0400

    Contract Network Stuff

--- src/contractnetwork.jl
+++ src/contractnetwork.jl
@@ -1,15 +1,6 @@
-using TensorOperations: TensorOperations, optimaltree
 using ITensorBase: inds, dim
 
-default_sequence_alg = "optimal"
-
-function contraction_sequence(::Algorithm"optimal", tn::Vector{<:AbstractArray})
-  network = collect.(inds.(tn))
-  #Converting dims to Float64 to minimize overflow issues
-  inds_to_dims = Dict(i => Float64(dim(i)) for i in unique(reduce(vcat, network)))
-  seq, _ = optimaltree(network, inds_to_dims)
-  return seq
-end
+default_sequence_alg = "leftassociative"
 
 function contraction_sequence(::Algorithm"leftassociative", tn::Vector{<:AbstractArray})
   return Any[i for i in 1:length(tn)]
@@ -20,8 +11,8 @@ function contraction_sequence(tn::Vector{<:AbstractArray}; alg=default_sequence_
 end
 
 # Internal recursive worker
-function recursive_contractnetwork(tn::Union{AbstractVector,AbstractArray})
-  tn isa AbstractVector && return reduce(*, map(recursive_contractnetwork, tn))
+function recursive_contractnetwork(tn::Union{AbstractVector,AbstractNamedDimsArray})
+  tn isa AbstractVector && return prod(recursive_contractnetwork, tn)
   return tn
 end
 
@@ -29,11 +20,11 @@ end
 rearrange(tn::Vector{<:AbstractArray}, i::Integer) = tn[i]
 rearrange(tn::Vector{<:AbstractArray}, v::AbstractVector) = [rearrange(tn, s) for s in v]
 
-function contractnetwork(tn::Vector{<:AbstractArray}; sequence_alg=default_sequence_alg)
-  sequence = contraction_sequence(tn; alg=sequence_alg)
-  return recursive_contractnetwork(rearrange(tn, sequence))
+function contractnetwork(tn::Vector{<:AbstractArray}; sequence=default_sequence_alg)
+  contract_sequence = isa(sequence, String) ? contraction_sequence(tn; alg=sequence) : sequence
+  return recursive_contractnetwork(rearrange(tn, contract_sequence))
 end
 
-function contractnetwork(tn::AbstractTensorNetwork; sequence_alg=default_sequence_alg)
-  return contractnetwork([tn[v] for v in vertices(tn)]; sequence_alg)
+function contractnetwork(tn::AbstractTensorNetwork; sequence=default_sequence_alg)
+  return contractnetwork([tn[v] for v in vertices(tn)]; sequence)
 end
