0bbf58450efc0033daaa477290ba539e016e1983
Jack Dunham <jdunham@flatironinstitute.org>
2025-10-28 15:12:37 -0400

    Reintroduce `AbstractNetworkIterator` and `AbstractProblem` interface

--- src/ITensorNetworksNext.jl
+++ src/ITensorNetworksNext.jl
@@ -4,6 +4,8 @@ include("lazynameddimsarrays.jl")
 include("abstracttensornetwork.jl")
 include("tensornetwork.jl")
 include("contract_network.jl")
+include("abstract_problem.jl")
+include("iterators.jl")
 
 include("beliefpropagation/abstractbeliefpropagationcache.jl")
 include("beliefpropagation/beliefpropagationcache.jl")
