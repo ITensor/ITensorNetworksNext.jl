69a2e7ef66e76036767b458752a4987677f51595
Jack Dunham <jdunham@flatironinstitute.org>
2025-10-28 15:12:37 -0400

    Reintroduce `AbstractNetworkIterator` and `AbstractProblem` interface

--- src/ITensorNetworksNext.jl
+++ src/ITensorNetworksNext.jl
@@ -2,6 +2,8 @@ module ITensorNetworksNext
 
 include("abstracttensornetwork.jl")
 include("tensornetwork.jl")
+include("abstract_problem.jl")
+include("iterators.jl")
 
 include("beliefpropagation/abstractbeliefpropagationcache.jl")
 include("beliefpropagation/beliefpropagationcache.jl")
