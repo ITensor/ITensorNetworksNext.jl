bf0780fb848feec626b2e8cedf7a715a02b3bc33
mtfishman <mfishman@flatironinstitute.org>
2025-11-06 10:46:38 -0500

    More Ising tests

--- test/test_tensornetworkgenerators.jl
+++ test/test_tensornetworkgenerators.jl
@@ -11,13 +11,41 @@ using Test: @test, @testset
 module TestUtils
     using QuadGK: quadgk
     # Exact critical inverse temperature for 2D square lattice Ising model.
-    βc() = 0.5 * log(1 + √2)
+    βc_2d_ising(elt::Type{<:Number} = Float64) = elt(log(1 + √2) / 2)
     # Exact infinite volume free energy density for 2D square lattice Ising model.
-    function ising_free_energy_density(β::Real)
-        κ = 2sinh(2β) / cosh(2β)^2
-        integrand(θ) = log(0.5 * (1 + sqrt(abs(1 - (κ * sin(θ))^2))))
+    function f_2d_ising(β::Real; J::Real = one(β))
+        κ = 2sinh(2β * J) / cosh(2β * J)^2
+        integrand(θ) = log((1 + √(abs(1 - (κ * sin(θ))^2))) / 2)
         integral, _ = quadgk(integrand, 0, π)
-        return (-log(2cosh(2β)) - (1 / (2π)) * integral) / β
+        return (-log(2cosh(2β * J)) - (1 / (2π)) * integral) / β
+    end
+    function f_1d_ising(β::Real; J::Real = one(β), h::Real = zero(β))
+        λ⁺ = exp(β * J) * (cosh(β * h) + √(sinh(β * h)^2 + exp(-4β * J)))
+        return -(log(λ⁺) / β)
+    end
+    function f_1d_ising(β::Real, N::Integer; periodic::Bool = true, kwargs...)
+        return if periodic
+            f_1d_ising_periodic(β, N; kwargs...)
+        else
+            f_1d_ising_open(β, N; kwargs...)
+        end
+    end
+    function f_1d_ising_periodic(β::Real, N::Integer; J::Real = one(β), h::Real = zero(β))
+        r = √(sinh(β * h)^2 + exp(-4β * J))
+        λ⁺ = exp(β * J) * (cosh(β * h) + r)
+        λ⁻ = exp(β * J) * (cosh(β * h) - r)
+        Z = λ⁺^N + λ⁻^N
+        return -(log(Z) / (β * N))
+    end
+    function f_1d_ising_open(β::Real, N::Integer; J::Real = one(β), h::Real = zero(β))
+        isone(N) && return 2cosh(β * h)
+        T = [
+            exp(β * (J + h)) exp(-β * J);
+            exp(-β * J) exp(β * (J - h));
+        ]
+        b = [exp(β * h / 2), exp(-β * h / 2)]
+        Z = (b' * (T^(N - 1)) * b)[]
+        return -(log(Z) / (β * N))
     end
 end
 
@@ -38,25 +66,49 @@ end
         end
     end
     @testset "Ising Network" begin
-        dims = (4, 4)
-        β = TestUtils.βc()
-        g = named_grid(dims; periodic = true)
-        ldict = Dict(e => Index(2) for e in edges(g))
-        l(e) = get(() -> ldict[reverse(e)], ldict, e)
-        tn = ising_network(l, β, g)
-        @test nv(tn) == 16
-        @test ne(tn) == ne(g)
-        @test issetequal(vertices(tn), vertices(g))
-        @test issetequal(arranged_edges(tn), arranged_edges(g))
-        for v in vertices(tn)
-            is = l.(incident_edges(g, v))
-            @test issetequal(is, inds(tn[v]))
-            @test tn[v] ≠ δ(Tuple(is))
+        @testset "1D Ising (periodic = $periodic)" for periodic in (false, true)
+            dims = (4,)
+            β = 0.4
+            g = named_grid(dims; periodic)
+            ldict = Dict(e => Index(2) for e in edges(g))
+            l(e) = get(() -> ldict[reverse(e)], ldict, e)
+            tn = ising_network(l, β, g)
+            @test nv(tn) == 4
+            @test ne(tn) == ne(g)
+            @test issetequal(vertices(tn), vertices(g))
+            @test issetequal(arranged_edges(tn), arranged_edges(g))
+            for v in vertices(tn)
+                is = l.(incident_edges(g, v))
+                @test issetequal(is, inds(tn[v]))
+                @test tn[v] ≠ δ(Tuple(is))
+            end
+            # TODO: Use eager contraction sequence finding.
+            z = contract_network(tn; alg = "exact")[]
+            f = -log(z) / (β * nv(g))
+            f_analytic = TestUtils.f_1d_ising(β, 4; periodic)
+            @test f ≈ f_analytic
         end
-        # TODO: Use eager contraction sequence finding.
-        z = contract_network(tn; alg = "exact")[]
-        f = -log(z) / (β * nv(g))
-        f_inf = TestUtils.ising_free_energy_density(β)
-        @test f ≈ f_inf rtol = 1.0e-1
+        @testset "2D Ising" begin
+            dims = (4, 4)
+            β = TestUtils.βc_2d_ising()
+            g = named_grid(dims; periodic = true)
+            ldict = Dict(e => Index(2) for e in edges(g))
+            l(e) = get(() -> ldict[reverse(e)], ldict, e)
+            tn = ising_network(l, β, g)
+            @test nv(tn) == 16
+            @test ne(tn) == ne(g)
+            @test issetequal(vertices(tn), vertices(g))
+            @test issetequal(arranged_edges(tn), arranged_edges(g))
+            for v in vertices(tn)
+                is = l.(incident_edges(g, v))
+                @test issetequal(is, inds(tn[v]))
+                @test tn[v] ≠ δ(Tuple(is))
+            end
+            # TODO: Use eager contraction sequence finding.
+            z = contract_network(tn; alg = "exact")[]
+            f = -log(z) / (β * nv(g))
+            f_inf = TestUtils.f_2d_ising(β)
+            @test f ≈ f_inf rtol = 1.0e-1
+        end
     end
 end
