776b90b2201e428c32223d35116e72cd27c96343
mtfishman <mfishman@flatironinstitute.org>
2025-11-05 20:31:01 -0500

    Add definitions of delta and Ising networks

---
+++ src/TensorNetworkGenerators/delta_network.jl
@@ -0,0 +1,21 @@
+using DiagonalArrays: δ
+using Graphs: AbstractGraph
+using ..ITensorNetworksNext: TensorNetwork
+using NamedGraphs.GraphsExtensions: incident_edges
+
+"""
+    delta_network(f, elt::Type = Float64, g::AbstractGraph)
+
+Construct a TensorNetwork on the graph `g` with element type `elt` that has delta tensors
+on each vertex. Link dimensions are defined using the function `f(e)` that should take an
+edge `e` as an input and should output the link index on that edge.
+"""
+function delta_network(f, elt::Type, g::AbstractGraph)
+    return tn = TensorNetwork(g) do v
+        is = Tuple(f.(incident_edges(g, v)))
+        return δ(elt, is)
+    end
+end
+function delta_network(f, g::AbstractGraph)
+    return delta_network(f, Float64, g)
+end
