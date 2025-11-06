85e04ebf70381e668245412c653482180c5e97ef
mtfishman <mfishman@flatironinstitute.org>
2025-11-06 17:05:58 -0500

    Refactor

--- ext/ITensorNetworksNextTensorOperationsExt/ITensorNetworksNextTensorOperationsExt.jl
+++ ext/ITensorNetworksNextTensorOperationsExt/ITensorNetworksNextTensorOperationsExt.jl
@@ -1,16 +1,24 @@
 module ITensorNetworksNextTensorOperationsExt
 
 using BackendSelection: @Algorithm_str, Algorithm
+using ITensorNetworksNext: ITensorNetworksNext, contraction_order
 using NamedDimsArrays: inds
-using ITensorNetworksNext: ITensorNetworksNext, contraction_sequence_to_expr
 using TensorOperations: TensorOperations, optimaltree
 
-function ITensorNetworksNext.contraction_sequence(::Algorithm"optimal", tn::Vector{<:AbstractArray})
-    network = collect.(inds.(tn))
-    #Converting dims to Float64 to minimize overflow issues
+function contraction_order_to_expr(seq)
+    return seq isa AbstractVector ? prod(contraction_order_to_expr, seq) : symnameddims(seq)
+end
+
+function ITensorNetworksNext.contraction_order(alg::Algorithm"optimal", tn)
+    ts = [tn[i] for i in eachindex(tn)]
+    network = collect.(inds.(ts))
+    # Converting dims to Float64 to minimize overflow issues
     inds_to_dims = Dict(i => Float64(length(i)) for i in unique(reduce(vcat, network)))
     seq, _ = optimaltree(network, inds_to_dims)
-    return contraction_sequence_to_expr(seq)
+    # TODO: Map the integer indices back to the original tensor network vertices.
+    expr = contraction_order_to_expr(seq)
+    subs = Dict(symnameddims(i) => symnameddims(eachindex(tn)[i]) for i in eachindex(ts))
+    return substitute(expr, subs)
 end
 
 end
--- src/contract_network.jl
+++ src/contract_network.jl
@@ -13,37 +13,29 @@ function select_algorithm(alg; kwargs...)
     end
 end
 
-# TODO: Use more general `default_kwargs(...)` here?
+# `contract_network`
 function default_kwargs(::typeof(contract_network), tn)
-    return (; alg = Algorithm"exact"(; evaluation_order_alg = Algorithm"eager"()))
+    return (; alg = Algorithm"exact"(; order_alg = Algorithm"eager"()))
 end
 function contract_network(tn; alg = default_kwargs(contract_network, tn).alg, kwargs...)
     return contract_network(select_algorithm(alg; kwargs...), tn)
 end
-
 function contract_network(alg::Algorithm, tn::AbstractTensorNetwork)
     return error("Not implemented.")
 end
+
+# `contract_network(::Algorithm"exact", ...)`
 function contract_network(alg::Algorithm"exact", tn)
-    evaluation_order = @something begin
-        get(alg, :evaluation_order, nothing)
-        contraction_order(tn; alg = alg.evaluation_order_alg)
+    order = @something begin
+        get(alg, :order, nothing)
+        contraction_order(tn; alg = alg.order_alg)
     end
     syms_to_ts = Dict(symnameddims(i) => lazy(tn[i]) for i in eachindex(tn))
-    tn_expression = substitute(evaluation_order, syms_to_ts)
+    tn_expression = substitute(order, syms_to_ts)
     return materialize(tn_expression)
 end
 
-# TODO: Move to TensorOperationsExt.
-function contraction_order_to_expr(seq)
-    if seq isa AbstractVector
-        return prod(contraction_order_to_expr, seq)
-    else
-        return symnameddims(seq)
-    end
-end
-
-# TODO: Use more general `default_kwargs(...)` here?
+# `contraction_order`
 default_kwargs(::typeof(contraction_order), tn) = (; alg = Algorithm"eager"())
 function contraction_order(tn; alg = default_kwargs(contraction_order, tn).alg, kwargs...)
     return contraction_order(select_algorithm(alg; kwargs...), tn)
@@ -51,6 +43,8 @@ end
 function contraction_order(alg::Algorithm, tn)
     return error("Not implemented.")
 end
+
+# `contraction_order(::Algorithm"eager", ...)`
 function contraction_order(alg::Algorithm"eager", tn)
     return error("Eager not implemented.")
 end
