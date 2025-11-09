2ada7cd8b6eff1d57c77558288ab854e49ed6eb4
mtfishman <mfishman@flatironinstitute.org>
2025-11-08 21:31:45 -0500

    Fix tests

--- ext/ITensorNetworksNextTensorOperationsExt/ITensorNetworksNextTensorOperationsExt.jl
+++ ext/ITensorNetworksNextTensorOperationsExt/ITensorNetworksNextTensorOperationsExt.jl
@@ -2,22 +2,25 @@ module ITensorNetworksNextTensorOperationsExt
 
 using BackendSelection: @Algorithm_str, Algorithm
 using ITensorNetworksNext: ITensorNetworksNext, contraction_order
+using ITensorNetworksNext.LazyNamedDimsArrays: symnameddims, substitute
 using NamedDimsArrays: inds
 using TensorOperations: TensorOperations, optimaltree
 
-function contraction_order_to_expr(seq)
-    return seq isa AbstractVector ? prod(contraction_order_to_expr, seq) : symnameddims(seq)
+function contraction_order_to_expr(ord)
+    return ord isa AbstractVector ? prod(contraction_order_to_expr, ord) : symnameddims(ord)
 end
 
 function ITensorNetworksNext.contraction_order(alg::Algorithm"optimal", tn)
-    ts = [tn[i] for i in eachindex(tn)]
+    ts = [tn[i] for i in keys(tn)]
     network = collect.(inds.(ts))
     # Converting dims to Float64 to minimize overflow issues
     inds_to_dims = Dict(i => Float64(length(i)) for i in unique(reduce(vcat, network)))
-    seq, _ = optimaltree(network, inds_to_dims)
+    order, _ = optimaltree(network, inds_to_dims)
     # TODO: Map the integer indices back to the original tensor network vertices.
-    expr = contraction_order_to_expr(seq)
-    subs = Dict(symnameddims(i) => symnameddims(eachindex(tn)[i]) for i in eachindex(ts))
+    expr = contraction_order_to_expr(order)
+    verts = collect(keys(tn))
+    sym(i) = symnameddims(verts[i], Tuple(inds(tn[verts[i]])))
+    subs = Dict(symnameddims(i) => sym(i) for i in eachindex(verts))
     return substitute(expr, subs)
 end
 
