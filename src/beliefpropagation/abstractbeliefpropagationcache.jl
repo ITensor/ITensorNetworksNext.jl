8bd8c3507832b236a42459c1b730ecee6c15ad25
Jack Dunham <jdunham@flatironinstitute.org>
2025-11-25 12:27:50 -0500

    BP Caching overhauls

--- src/beliefpropagation/abstractbeliefpropagationcache.jl
+++ src/beliefpropagation/abstractbeliefpropagationcache.jl
@@ -1,117 +1,124 @@
-abstract type AbstractBeliefPropagationCache{V} <: AbstractGraph{V} end
+using Graphs: AbstractGraph, AbstractEdge
+using DataGraphs: AbstractDataGraph, edge_data, vertex_data, edge_data_eltype
+using NamedGraphs.GraphsExtensions: boundary_edges
+using NamedGraphs.PartitionedGraphs: QuotientView, QuotientEdge, parent
 
-#Interface
-factor(bp_cache::AbstractBeliefPropagationCache, vertex) = not_implemented()
-setfactor!(bp_cache::AbstractBeliefPropagationCache, vertex, factor) = not_implemented()
-messages(bp_cache::AbstractBeliefPropagationCache) = not_implemented()
-message(bp_cache::AbstractBeliefPropagationCache, edge::AbstractEdge) = not_implemented()
-function default_message(bp_cache::AbstractBeliefPropagationCache, edge::AbstractEdge)
-    return not_implemented()
-end
-default_messages(bp_cache::AbstractBeliefPropagationCache) = not_implemented()
-function setmessage!(bp_cache::AbstractBeliefPropagationCache, edge::AbstractEdge, message)
-    return not_implemented()
-end
-function deletemessage!(bp_cache::AbstractBeliefPropagationCache, edge::AbstractEdge)
-    return not_implemented()
-end
-function rescale_messages(
-        bp_cache::AbstractBeliefPropagationCache, edges::Vector{<:AbstractEdge}; kwargs...
-    )
-    return not_implemented()
-end
-function rescale_vertices(
-        bp_cache::AbstractBeliefPropagationCache, vertices::Vector; kwargs...
-    )
-    return not_implemented()
+messages(::AbstractGraph) = not_implemented()
+messages(bp_cache::AbstractDataGraph) = edge_data(bp_cache)
+messages(bp_cache::AbstractGraph, edges) = [message(bp_cache, e) for e in edges]
+
+message(bp_cache::AbstractGraph, edge::AbstractEdge) = messages(bp_cache)[edge]
+
+deletemessage!(bp_cache::AbstractGraph, edge) = not_implemented()
+function deletemessage!(bp_cache::AbstractDataGraph, edge)
+    ms = messages(bp_cache)
+    delete!(ms, edge)
+    return bp_cache
 end
 
-function vertex_scalar(bp_cache::AbstractBeliefPropagationCache, vertex; kwargs...)
-    return not_implemented()
-end
-function edge_scalar(
-        bp_cache::AbstractBeliefPropagationCache, edge::AbstractEdge; kwargs...
-    )
-    return not_implemented()
-end
-
-#Graph functionality needed
-Graphs.vertices(bp_cache::AbstractBeliefPropagationCache) = not_implemented()
-Graphs.edges(bp_cache::AbstractBeliefPropagationCache) = not_implemented()
-function NamedGraphs.GraphsExtensions.boundary_edges(
-        bp_cache::AbstractBeliefPropagationCache, vertices; kwargs...
-    )
-    return not_implemented()
-end
-
-#Functions derived from the interface
-function setmessages!(bp_cache::AbstractBeliefPropagationCache, edges, messages)
-    for (e, m) in zip(edges)
-        setmessage!(bp_cache, e, m)
-    end
-    return
-end
-
-function deletemessages!(
-        bp_cache::AbstractBeliefPropagationCache, edges::Vector{<:AbstractEdge} = edges(bp_cache)
-    )
+function deletemessages!(bp_cache::AbstractGraph, edges = edges(bp_cache))
     for e in edges
         deletemessage!(bp_cache, e)
     end
     return bp_cache
 end
 
-function vertex_scalars(
-        bp_cache::AbstractBeliefPropagationCache, vertices = Graphs.vertices(bp_cache); kwargs...
-    )
-    return map(v -> region_scalar(bp_cache, v; kwargs...), vertices)
+setmessage!(bp_cache::AbstractGraph, edge, message) = not_implemented()
+function setmessage!(bp_cache::AbstractDataGraph, edge, message)
+    ms = messages(bp_cache)
+    set!(ms, edge, message)
+    return bp_cache
+end
+function setmessage!(bp_cache::QuotientView, edge, message)
+    setmessages!(parent(bp_cache), QuotientEdge(edge), message)
+    return bp_cache
 end
 
-function edge_scalars(
-        bp_cache::AbstractBeliefPropagationCache, edges = Graphs.edges(bp_cache); kwargs...
-    )
-    return map(e -> region_scalar(bp_cache, e; kwargs...), edges)
+function setmessages!(bp_cache::AbstractGraph, edge::QuotientEdge, message)
+    for e in edges(bp_cache, edge)
+        setmessage!(parent(bp_cache), e, message[e])
+    end
+    return bp_cache
+end
+function setmessages!(bpc_dst::AbstractGraph, bpc_src::AbstractGraph, edges)
+    for e in edges
+        setmessage!(bpc_dst, e, message(bpc_src, e))
+    end
+    return bpc_dst
 end
 
-function scalar_factors_quotient(bp_cache::AbstractBeliefPropagationCache)
+factors(bpc::AbstractGraph) = vertex_data(bpc)
+factors(bpc::AbstractGraph, vertices::Vector) = [factor(bpc, v) for v in vertices]
+factors(bpc::AbstractGraph{V}, vertex::V) where {V} = factors(bpc, V[vertex])
+
+factor(bpc::AbstractGraph, vertex) = factors(bpc)[vertex]
+
+setfactor!(bpc::AbstractGraph, vertex, factor) = not_implemented()
+function setfactor!(bpc::AbstractDataGraph, vertex, factor)
+    fs = factors(bpc)
+    set!(fs, vertex, factor)
+    return bpc
+end
+
+function region_scalar(bp_cache::AbstractGraph, edge::AbstractEdge)
+    return message(bp_cache, edge) * message(bp_cache, reverse(edge))
+end
+
+function region_scalar(bp_cache::AbstractGraph, vertex)
+
+    messages = incoming_messages(bp_cache, vertex)
+    state = factors(bp_cache, vertex)
+
+    return reduce(*, messages) * reduce(*, state)
+end
+
+message_type(bpc::AbstractGraph) = message_type(typeof(bpc))
+message_type(G::Type{<:AbstractGraph}) = eltype(Base.promote_op(messages, G))
+message_type(type::Type{<:AbstractDataGraph}) = edge_data_eltype(type)
+
+function vertex_scalars(bp_cache::AbstractGraph, vertices = vertices(bp_cache))
+    return map(v -> region_scalar(bp_cache, v), vertices)
+end
+
+function edge_scalars(bp_cache::AbstractGraph, edges = edges(bp_cache))
+    return map(e -> region_scalar(bp_cache, e), edges)
+end
+
+function scalar_factors_quotient(bp_cache::AbstractGraph)
     return vertex_scalars(bp_cache), edge_scalars(bp_cache)
 end
 
-function incoming_messages(
-        bp_cache::AbstractBeliefPropagationCache, vertices::Vector{<:Any}; ignore_edges = []
-    )
-    b_edges = NamedGraphs.GraphsExtensions.boundary_edges(bp_cache, vertices; dir = :in)
+function incoming_messages(bp_cache::AbstractGraph, vertices; ignore_edges = [])
+    b_edges = boundary_edges(bp_cache, [vertices;]; dir = :in)
     b_edges = !isempty(ignore_edges) ? setdiff(b_edges, ignore_edges) : b_edges
     return messages(bp_cache, b_edges)
 end
 
-function incoming_messages(bp_cache::AbstractBeliefPropagationCache, vertex; kwargs...)
-    return incoming_messages(bp_cache, [vertex]; kwargs...)
-end
+default_messages(::AbstractGraph) = not_implemented()
 
 #Adapt interface for changing device
-function map_messages(f, bp_cache::AbstractBeliefPropagationCache, es = edges(bp_cache))
-    bp_cache = copy(bp_cache)
+map_messages(f, bp_cache, es = edges(bp_cache)) = map_messages!(f, copy(bp_cache), es)
+function map_messages!(f, bp_cache, es = edges(bp_cache))
     for e in es
         setmessage!(bp_cache, e, f(message(bp_cache, e)))
     end
     return bp_cache
 end
-function map_factors(f, bp_cache::AbstractBeliefPropagationCache, vs = vertices(bp_cache))
-    bp_cache = copy(bp_cache)
+
+map_factors(f, bp_cache, vs = vertices(bp_cache)) = map_factors!(f, copy(bp_cache), vs)
+function map_factors!(f, bp_cache, vs = vertices(bp_cache))
     for v in vs
         setfactor!(bp_cache, v, f(factor(bp_cache, v)))
     end
     return bp_cache
 end
-function adapt_messages(to, bp_cache::AbstractBeliefPropagationCache, args...)
-    return map_messages(adapt(to), bp_cache, args...)
-end
-function adapt_factors(to, bp_cache::AbstractBeliefPropagationCache, args...)
-    return map_factors(adapt(to), bp_cache, args...)
-end
 
-function freenergy(bp_cache::AbstractBeliefPropagationCache)
+adapt_messages(to, bp_cache, es = edges(bp_cache)) = map_messages(adapt(to), bp_cache, es)
+adapt_factors(to, bp_cache, vs = vertices(bp_cache)) = map_factors(adapt(to), bp_cache, vs)
+
+abstract type AbstractBeliefPropagationCache{V, ED} <: AbstractDataGraph{V, Nothing, ED} end
+
+function free_energy(bp_cache::AbstractBeliefPropagationCache)
     numerator_terms, denominator_terms = scalar_factors_quotient(bp_cache)
     if any(t -> real(t) < 0, numerator_terms)
         numerator_terms = complex.(numerator_terms)
@@ -123,29 +130,4 @@ function freenergy(bp_cache::AbstractBeliefPropagationCache)
     any(iszero, denominator_terms) && return -Inf
     return sum(log.(numerator_terms)) - sum(log.((denominator_terms)))
 end
-
-function partitionfunction(bp_cache::AbstractBeliefPropagationCache)
-    return exp(freenergy(bp_cache))
-end
-
-function rescale_messages(bp_cache::AbstractBeliefPropagationCache, edge::AbstractEdge)
-    return rescale_messages(bp_cache, [edge])
-end
-
-function rescale_messages(bp_cache::AbstractBeliefPropagationCache)
-    return rescale_messages(bp_cache, edges(bp_cache))
-end
-
-function rescale_vertices(bpc::AbstractBeliefPropagationCache; kwargs...)
-    return rescale_vertices(bpc, collect(vertices(bpc)); kwargs...)
-end
-
-function rescale_vertex(bpc::AbstractBeliefPropagationCache, vertex; kwargs...)
-    return rescale_vertices(bpc, [vertex]; kwargs...)
-end
-
-function rescale(bpc::AbstractBeliefPropagationCache, args...; kwargs...)
-    bpc = rescale_messages(bpc)
-    bpc = rescale_partitions(bpc, args...; kwargs...)
-    return bpc
-end
+partitionfunction(bp_cache::AbstractBeliefPropagationCache) = exp(free_energy(bp_cache))
--- src/beliefpropagation/beliefpropagationcache.jl
+++ src/beliefpropagation/beliefpropagationcache.jl
@@ -1,145 +1,93 @@
+using DataGraphs: DataGraphs, AbstractDataGraph, DataGraph
 using Dictionaries: Dictionary, set!, delete!
 using Graphs: AbstractGraph, is_tree, connected_components
-using NamedGraphs.GraphsExtensions: default_root_vertex, forest_cover, post_order_dfs_edges
+using NamedGraphs: convert_vertextype
+using NamedGraphs.GraphsExtensions: default_root_vertex, forest_cover, post_order_dfs_edges, is_path_graph
 using ITensorBase: ITensor, dim
+using NamedGraphs.PartitionedGraphs: QuotientView, QuotientEdge, quotient_graph
 
-struct BeliefPropagationCache{V, N <: AbstractDataGraph{V}} <:
-    AbstractBeliefPropagationCache{V}
+struct BeliefPropagationCache{V, N <: AbstractGraph{V}, ET, MT} <:
+    AbstractBeliefPropagationCache{V, MT}
     network::N
-    messages::Dictionary
+    messages::Dictionary{ET, MT}
 end
 
-messages(bp_cache::BeliefPropagationCache) = bp_cache.messages
-network(bp_cache::BeliefPropagationCache) = bp_cache.network
+network(bp_cache) = underlying_graph(bp_cache)
 
-BeliefPropagationCache(network) = BeliefPropagationCache(network, Dictionary())
+DataGraphs.underlying_graph(bpc::BeliefPropagationCache) = getfield(bpc, :network)
+DataGraphs.edge_data(bpc::BeliefPropagationCache) = getfield(bpc, :messages)
+DataGraphs.vertex_data(bpc::BeliefPropagationCache) = vertex_data(network(bpc))
+function DataGraphs.underlying_graph_type(type::Type{<:BeliefPropagationCache})
+    return fieldtype(type, :network)
+end
+
+message_type(::Type{<:BeliefPropagationCache{V, N, ET, MT}}) where {V, N, ET, MT} = MT
+
+function BeliefPropagationCache(alg, network::AbstractGraph)
+    es = collect(edges(network))
+    es = vcat(es, reverse.(es))
+    messages = map(edge -> default_message(alg, network, edge), es)
+    return BeliefPropagationCache(network, Dictionary(es, messages))
+end
 
 function Base.copy(bp_cache::BeliefPropagationCache)
     return BeliefPropagationCache(copy(network(bp_cache)), copy(messages(bp_cache)))
 end
 
-function deletemessage!(bp_cache::BeliefPropagationCache, e::AbstractEdge)
-    ms = messages(bp_cache)
-    delete!(ms, e)
-    return bp_cache
+# TODO: This needs to go in DataGraphsGraphsExtensionsExt
+#
+# This function is problematic when `ng isa TensorNetwork` as it relies on deleting edges
+# and taking subgraphs, which is not always well-defined for the `TensorNetwork` type,
+# hence we just strip off any `AbstractDataGraph` data to avoid this.
+function forest_cover_edge_sequence(g::AbstractDataGraph; kwargs...)
+    return forest_cover_edge_sequence(underlying_graph(g); kwargs...)
 end
-
-function setmessage!(bp_cache::BeliefPropagationCache, e::AbstractEdge, message)
-    ms = messages(bp_cache)
-    set!(ms, e, message)
-    return bp_cache
+# TODO: This needs to go in PartitionedGraphsGraphsExtensionsExt
+#
+# While it is not at all necessary to explictly instantiate the `QuotientView`, it allows the
+# data of a data graph to be removed using the above method if `parent_type(g)` is an
+# `AbstractDataGraph`.
+function forest_cover_edge_sequence(g::QuotientView; kwargs...)
+    return forest_cover_edge_sequence(quotient_graph(parent(g)); kwargs...)
 end
-
-function setmessages!(bpc_dst::BeliefPropagationCache, bpc_src::BeliefPropagationCache, edges)
-    ms_dst = messages(bpc_dst)
-    for e in edges
-        set!(ms_dst, e, message(bpc_src, e))
-    end
-    return bpc_dst
-end
-
-function message(bp_cache::BeliefPropagationCache, edge::AbstractEdge; kwargs...)
-    ms = messages(bp_cache)
-    return get(() -> default_message(bp_cache, edge; kwargs...), ms, edge)
-end
-
-function messages(bp_cache::BeliefPropagationCache, edges::Vector{<:AbstractEdge})
-    return [message(bp_cache, e) for e in edges]
-end
-
-#Forward onto the network
-for f in [
-        :(Graphs.vertices),
-        :(Graphs.edges),
-        :(Graphs.is_tree),
-        :(NamedGraphs.GraphsExtensions.boundary_edges),
-        :(factors),
-        :(default_bp_maxiter),
-        :(ITensorNetworksNext.setfactor!),
-        :(ITensorNetworksNext.linkinds),
-        :(ITensorNetworksNext.underlying_graph),
-    ]
-    @eval begin
-        function $f(bp_cache::BeliefPropagationCache, args...; kwargs...)
-            return $f(network(bp_cache), args...; kwargs...)
-        end
-    end
-end
-
-function factors(tn::AbstractTensorNetwork, vertex)
-    return [tn[vertex]]
-end
-
-function region_scalar(bp_cache::BeliefPropagationCache, edge::AbstractEdge)
-    return (message(bp_cache, edge) * message(bp_cache, reverse(edge)))[]
-end
-
-function region_scalar(bp_cache::BeliefPropagationCache, vertex)
-    incoming_ms = incoming_messages(bp_cache, vertex)
-    state = factors(bp_cache, vertex)
-    return (reduce(*, incoming_ms) * reduce(*, state))[]
-end
-
-function default_message(bp_cache::BeliefPropagationCache, edge::AbstractEdge)
-    return default_message(network(bp_cache), edge::AbstractEdge)
-end
-
-function default_message(tn::AbstractTensorNetwork, edge::AbstractEdge)
-    t = ITensor(ones(dim.(linkinds(tn, edge))...), linkinds(tn, edge)...)
-    #TODO: Get datatype working on tensornetworks so we can support GPU, etc...
-    return t
-end
-
-#TODO: Update message etc should go here...
-function updated_message(
-        alg::Algorithm"contract", bp_cache::BeliefPropagationCache, edge::AbstractEdge
-    )
-    vertex = src(edge)
-    incoming_ms = incoming_messages(
-        bp_cache, vertex; ignore_edges = typeof(edge)[reverse(edge)]
-    )
-    state = factors(bp_cache, vertex)
-    #contract_list = ITensor[incoming_ms; state]
-    #sequence = contraction_sequence(contract_list; alg=alg.kwargs.sequence_alg)
-    #updated_messages = contract(contract_list; sequence)
-    updated_message =
-        !isempty(incoming_ms) ? reduce(*, state) * reduce(*, incoming_ms) : reduce(*, state)
-    if alg.normalize
-        message_norm = LinearAlgebra.norm(updated_message)
-        if !iszero(message_norm)
-            updated_message /= message_norm
-        end
-    end
-    return updated_message
-end
-
-function default_algorithm(
-        ::Type{<:Algorithm"contract"}; normalize = true, sequence_alg = "optimal"
-    )
-    return Algorithm("contract"; normalize, sequence_alg)
-end
-function default_algorithm(
-        ::Type{<:Algorithm"adapt_update"}; adapt, alg = default_algorithm(Algorithm"contract")
-    )
-    return Algorithm("adapt_update"; adapt, alg)
-end
-
-function update_message!(
-        message_update_alg::Algorithm, bpc::BeliefPropagationCache, edge::AbstractEdge
-    )
-    return setmessage!(bpc, edge, updated_message(message_update_alg, bpc, edge))
-end
-
-#Edge sequence stuff
+# TODO: This needs to go in GraphsExtensions
 function forest_cover_edge_sequence(g::AbstractGraph; root_vertex = default_root_vertex)
+    add_edges!(g, edges(g))
     forests = forest_cover(g)
-    edges = edgetype(g)[]
+    rv = edgetype(g)[]
     for forest in forests
         trees = [forest[vs] for vs in connected_components(forest)]
         for tree in trees
             tree_edges = post_order_dfs_edges(tree, root_vertex(tree))
-            push!(edges, vcat(tree_edges, reverse(reverse.(tree_edges)))...)
+            push!(rv, vcat(tree_edges, reverse(reverse.(tree_edges)))...)
         end
     end
-    return edges
+    return rv
+end
+
+function bpcache_induced_subgraph(graph, subvertices)
+    underlying_subgraph, vlist = Graphs.induced_subgraph(network(graph), subvertices)
+    subgraph = BeliefPropagationCache(underlying_subgraph, typeof(edge_data(graph))())
+    for e in edges(subgraph)
+        if isassigned(graph, e)
+            set!(edge_data(subgraph), e, graph[e])
+        end
+    end
+    return subgraph, vlist
+end
+
+function Graphs.induced_subgraph(graph::BeliefPropagationCache, subvertices)
+    return bpcache_induced_subgraph(graph, subvertices)
+end
+# For method ambiguity
+function Graphs.induced_subgraph(graph::BeliefPropagationCache{V}, subvertices::AbstractVector{V}) where {V <: Int}
+    return bpcache_induced_subgraph(graph, subvertices)
+end
+
+## PartitionedGraphs
+
+function PartitionedGraphs.quotientview(bpc::BeliefPropagationCache)
+    qview = QuotientView(network(bpc))
+    messages = edge_data(QuotientView(bpc))
+    return BeliefPropagationCache(qview, messages)
 end
