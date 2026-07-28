49623ce7cd707ead9bda6bc796fa0884cb2499ac
Matthew Fishman <mfishman@flatironinstitute.org>
2026-07-28 10:38:47 -0400

    Use bracket concatenation instead of `vcat` in `vertex_scalar`

--- src/beliefpropagation/messagecache.jl
+++ src/beliefpropagation/messagecache.jl
@@ -141,7 +141,7 @@ function vertex_scalar(factors, messages, vertex; kwargs...)
     # TODO: `contract_network` can't currently contract a mix of operator and plain operands, so
     # unwrap operator-valued messages with `state` first. Remove the `state.` once `contract_network`
     # handles operator operands.
-    tensors = vcat([factors[vertex]], state.(collect(in_messages)))
+    tensors = [[factors[vertex]]; state.(collect(in_messages))]
     return contract_network(tensors; kwargs...)[]
 end
 
